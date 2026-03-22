d63f7397aaea0e41ab1fe82be62c44a5605d981b
carlosdanielssantiago20-web <carlosdanielssantiago20@gmail.com>
2026-03-22 12:20:16 -0400

    Update and rename package.json to app/app/app/page.js

---
+++ app/app/app/page.js
@@ -0,0 +1,15 @@
+{
+   "Nome":  "financeiro",  "name":  "financeiro",
+ "Versão":  "1.0.0",  "version":  "1.0.0",
+ "Privado":  verdadeiro ,       } }  ,   "Privado" :   true,
+ "Rascritos"          :            { "Rascritos"          :            {
+ "Dev"          :            "próximo dev"          ,            "Dev"          :            "Próximo Dev",
+ "Construir"          :            "Próxima construção"          ,            "Construir"          :            "next build",
+ "Iniciar"          :            "Próximo começo" "Iniciar"          :            "Próximo começo"
+   }         ,           
+   "Dependências"         :           { 
+     "Próximo"         :           "Mais recente"         ,           
+ "REAGIR"         :           "último"         ,           
+ "React-dom"         :           "último" 
+   } 
+}
